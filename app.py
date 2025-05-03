22fbc896482b9c3d4e6351f07143d7208e1cd453
Allu Lavanya <lavanyaallu2019@gmail.com>
2025-05-03 23:56:44 +0530

    app

--- app.py
+++ app.py
@@ -36,5 +36,4 @@ def detect_emotion():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+app.run(host="0.0.0.0", port=5000, debug=True)
