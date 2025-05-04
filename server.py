9287b21f2878aa9e790f77896589675fb973f6e8
Allu Lavanya <lavanyaallu2019@gmail.com>
2025-05-04 11:38:31 +0530

    updated server

--- server.py
+++ server.py
@@ -253,4 +253,5 @@ async def delete_product(name: str = Form(...)):
 # ✅ Run server
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))  # fallback to 8080 locally
+    uvicorn.run(app, host="0.0.0.0", port=port)
