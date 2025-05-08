f56e08e2d063af4133adcd9d8cf93238adff85bf
Allu Lavanya <lavanyaallu2019@gmail.com>
2025-05-08 19:33:02 +0530

    hi

--- g.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyDIjjWhRzrRFZsKv_DwhUnpqa2Q8jSvUP8")
-
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
-)
-print(response.text)
